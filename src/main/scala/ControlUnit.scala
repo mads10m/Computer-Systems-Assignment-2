4eabd2fedf4866c2bbc6d0b6569124a4f21754fb
Mads Harder <mads987a@gmail.com>
2021-11-01 23:27:08 +0100

    Init control unit

--- src/main/scala/ControlUnit.scala
+++ src/main/scala/ControlUnit.scala
@@ -4,6 +4,15 @@ import chisel3.util._
 class ControlUnit extends Module {
   val io = IO(new Bundle {
     //Define the module interface here (inputs/outputs)
+    val opcode = Input(UInt(4.U))
+
+    val end = Output(Bool())
+    val BranchSEL = Output(UInt(2.U))
+    val loadFromMem = Output(Bool())
+    val registerWrite = Output(Bool())
+    val immediate = Output(Bool())
+    val ALUsel = Output(UInt(2.U))
+    val memoryWrite = Output(Bool())
   })
 
   //Implement this module here
