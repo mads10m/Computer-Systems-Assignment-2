3ded98907e63af380d1357215ba9d3b171ade6b7
Niels Kjær Ersbøll <nker@dtu.dk>
2021-11-02 00:11:14 +0100

    First implementation of cpuTopModule

--- src/main/scala/CPUTop.scala
+++ src/main/scala/CPUTop.scala
@@ -28,12 +28,48 @@ class CPUTop extends Module {
   val alu = Module(new ALU())
 
   //Connecting the modules
-  //programCounter.io.run := io.run
-  //programMemory.io.address := programCounter.io.programCounter
+  programCounter.io.run := io.run
+  programMemory.io.address := programCounter.io.programCounter
 
   ////////////////////////////////////////////
   //Continue here with your connections
   ////////////////////////////////////////////
+  controlUnit.io.opcode := programMemory.io.instructionRead(32,29)
+  programCounter.io.stop := controlUnit.io.end
+  programCounter.io.programCounterJump := programMemory.io.instructionRead(28,23)
+  
+  // Register input
+  when(controlUnit.io.branchInstruction){
+    registerFile.io.readReg1 := programMemory.io.instructionRead(22,19)
+    registerFile.io.readReg2 := programMemory.io.instructionRead(18,15)
+  }.otherwise{
+    registerFile.io.readReg1 := programMemory.io.instructionRead(24,21)
+    registerFile.io.readReg2 := programMemory.io.instructionRead(20,17)
+  }
+  registerFile.io.writeReg := programMemory.io.instructionRead(28,25)
+  registerFile.io.writeEnable := controlUnit.io.registerWrite
+
+  // ALU Input
+  alu.io.op1 := registerFile.io.data1
+  alu.io.sel := controlUnit.io.aluSel
+  alu.io.op2:= Mux(controlUnit.io.immediate, Cat(Fill(16."b0"),programMemory.io.instructionRead(20,16)), registerFile.io.data2)
+
+  // Program counter jump mux
+  switch (controlUnit.io.branchSel){
+    is("b00") {programCounter.io.jump := Bool(false)}
+    is("b01") {programCounter.io.jump := !alu.io.compRes}
+    is("b10") {programCounter.io.jump := alu.io.compRes}
+  }
+
+  // DATA MEMORY Input
+  dataMemory.io.address := registerFile.io.data2(15,0)
+  dataMemory.io.dataWrite := registerFile.io.data1
+  dataMemory.io.writeEnable := controlUnit.io.memoryWrite
+
+
+  // Register write data mux
+  registerFile.io.writeData := Mux(controlUnit.io.loadFromMem, dataMemory.io.dataRead, alu.io.res)
+
 
   //This signals are used by the tester for loading the program to the program memory, do not touch
   programMemory.io.testerAddress := io.testerProgMemAddress
